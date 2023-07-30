9f54acb47cc9d6d5eed492a1c5addff8cdba8c3f
wy-luke <wykj.luke@gmail.com>
2023-07-30 12:08:14 +0800

    style: update comments and echoed logs

--- InstallSD.sh
+++ InstallSD.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-echo "############ Check and install Homebrew ############"
+echo "############ Check and install Homebrew ##############"
+# Homebrew: The missing package manager for macOS
+# More: https://brew.sh/
 if ! command -v brew &>/dev/null; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
@@ -9,11 +11,17 @@ fi
 echo
 
 echo "############ Check and install micromamba ############"
+# About mamba: A super fast Python package and environment manager (compared to conda)
+# About mamba: A tiny version of the mamba. No base environment nor a default version of Python
+# More: https://mamba.readthedocs.io/en/latest/index.html
 if ! command -v micromamba &>/dev/null; then
+    # Install micromamba
     brew install micromamba
+    # Init micromamba
     micromamba shell init -s bash -p ~/micromamba
     source ~/.bash_profile
     source ~/.bashrc
+    # Config micromamba, add default channels.
     micromamba config append channels conda-forge
     micromamba config append channels nodefaults
     micromamba config set channel_priority strict
@@ -22,7 +30,9 @@ else
 fi
 echo
 
-echo "############ Check and install Git ############"
+echo "############ Check and install Git ###################"
+# Git: A free and open source distributed version control system
+# More: https://git-scm.com/
 if ! command -v git &>/dev/null; then
     brew install git
 else
@@ -30,28 +40,33 @@ else
 fi
 echo
 
-echo "############ Download code ############"
-# Check if sd folder exits
+echo "############ Download code ###########################"
+# Check if SD's folder exits
 if [ ! -d "stable-diffusion-webui" ]; then
     git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 else
     echo "Code already downloaded."
 fi
+# Enter the SD's folder
 cd stable-diffusion-webui
 echo
 
-echo "############ Create virtual env ############"
-# Activate micromamba
+echo "############ Create virtual env ######################"
+# Activate micromamba in current shell
 export MAMBA_ROOT_PREFIX="~/micromamba" # Optional
 eval "$(micromamba shell hook -s posix)"
 
+# TODO: Check if the env exits
 #if micromamba env list | grep ".*sd.*" >/dev/null 2>&1; then
 micromamba create -n sd python=3.10.6
 #fi
 micromamba activate sd
+# Activate pyvenv to update pip to avoid some errors
 python -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
+# Delete pip cache to avoid some errors
+pip cache purge
 
-# Install sd
+# Install Stable Diffusion
 ./webui.sh
