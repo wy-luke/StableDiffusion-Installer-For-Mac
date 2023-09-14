50fc61495143ffe315cc9f2c8b51628a687242be
wy-luke <li1078797083@qq.com>
2023-09-14 19:30:55 +0800

    feat: add -c argument

--- sd-installer.sh
+++ sd-installer.sh
@@ -6,7 +6,7 @@ set -u
 installation_path=$HOME
 tmp_path="$HOME/.sd-installer"
 mamba_root_path="~/micromamba"
-net_connected=true
+network_connected=true
 
 # Other variables
 code_path="$installation_path/stable-diffusion-webui"
@@ -20,7 +20,7 @@ fi
 brew_path="$brew_pkg_path/bin/brew"
 mamba_path="$brew_pkg_path/opt/micromamba/bin/micromamba"
 
-test_mode=0 # Only for test. 0 for no test, 1 for yes-test, 2 for no-test
+test_mode=0 # Only for test. 0 for no test, 1 for Network-Connected-Test, 2 for Network-Not-Connected-Test
 
 function clean_up {
     echo "############ Clean ###################################"
@@ -47,7 +47,12 @@ function handle_error {
     error_choice=${error_choice:-y}
     if [[ $error_choice == [yY] ]]; then
         # Retry the command
-        /bin/bash -c "$(curl -fsSL $sd_installer_url)"
+
+        if [[ $network_connected == false ]]; then
+            /bin/bash -c "$(curl -fsSL $sd_installer_url)" -c
+        else
+            /bin/bash -c "$(curl -fsSL $sd_installer_url)"
+        fi
     else
         clean_up
         # Exit the script
@@ -70,8 +75,18 @@ function verify_installation {
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
     case "$1" in
-    -1 | -2)
-        test_mode=${1:1}
+    -test | -t)
+        value =${2:1}
+        if value != 1 && value != 2; then
+            echo_red "未知选项: $1 $2"
+            exit 1
+        fi
+        test_mode=value
+        shift # past argument
+        shift # past value
+        ;;
+    -c)
+        network_connected=false
         shift
         ;;
     # -h | --help)
@@ -88,21 +103,27 @@ done
 
 if [ "$test_mode" != 0 ]; then
     if [ "$test_mode" == 1 ]; then
-        net_connected=true
+        network_connected=true
         echo_green "Yes-test"
     else
-        net_connected=false
+        network_connected=false
         echo_green "No-test"
     fi
 else
-    echo_green "For non-Chinese users, you could just ignore this and press the Enter key"
-    read -rp "网络是否顺畅? 默认y [y/n] " net_choice
-    net_choice=${net_choice:-y}
-    if [[ $net_choice == [nN] ]]; then
-        net_connected=false
-        echo_green "将设置国内镜像源安装, 包括 homebrew、conda-forge、pip、github 等, 但链接可能会不稳定/失效, 若失败可以重试"
+    # echo_green "For non-Chinese users, you could just ignore this and press the Enter key"
+    # read -rp "网络是否顺畅? 默认y [y/n] " network_choice
+    # network_choice=${network_choice:-y}
+    # if [[ $network_choice == [nN] ]]; then
+    #     network_connected=false
+    #     echo_green "将设置国内镜像源安装, 包括 homebrew、conda-forge、pip、github 等, 但链接可能会不稳定/失效, 若失败可以重试"
+    # else
+    #     echo_green "网络通畅, 正常安装"
+    # fi
+
+    if [[ $network_connected == false ]]; then
+        echo_green "将设置国内镜像源安装, 包括 homebrew、conda-forge、pip、github 等, 但链接可能会不稳定或失效, 若安装失败可以重试"
     else
-        echo_green "网络通畅, 正常安装"
+        echo_green "恭喜网络通畅, 正常安装"
     fi
 fi
 
@@ -110,7 +131,7 @@ brew_installer_url="https://raw.githubusercontent.com/Homebrew/install/HEAD/inst
 sd_installer_url="https://raw.githubusercontent.com/wy-luke/StableDiffusion-Installer-For-Mac/main/sd-installer.sh"
 sd_webui_url="https://github.com/AUTOMATIC1111/stable-diffusion-webui.git"
 
-if ! $net_connected; then
+if ! $network_connected; then
     brew_installer_url="https://raw.fastgit.org/Homebrew/install/HEAD/install.sh"
     sd_installer_url="https://raw.fastgit.org/wy-luke/StableDiffusion-Installer-For-Mac/main/sd-installer.sh"
     sd_webui_url="https://ghproxy.com/github.com/AUTOMATIC1111/stable-diffusion-webui.git"
@@ -139,7 +160,7 @@ if ! command -v brew &>/dev/null; then
         # Grant the permission to execute the installation script
         chmod +x $brew_installer_path
 
-        if ! $net_connected; then
+        if ! $network_connected; then
             export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
             export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
             export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
@@ -149,7 +170,7 @@ if ! command -v brew &>/dev/null; then
         eval "$($brew_path shellenv)"
         verify_installation brew
 
-        if ! $net_connected; then
+        if ! $network_connected; then
             export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
             export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
             export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
@@ -193,7 +214,7 @@ else
 fi
 
 # Set default channels for micromamba
-if ! $net_connected; then
+if ! $network_connected; then
     micromamba config prepend channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/
     # micromamba config prepend channels http://mirrors.ustc.edu.cn/anaconda/cloud/conda-forge/
 else
@@ -230,7 +251,7 @@ fi
 # Enter the SD's folder
 cd $code_path
 
-if ! $net_connected; then
+if ! $network_connected; then
     sed -i '' "s/https:\/\/github.com/https:\/\/ghproxy.com\/github.com/g" modules/launch_utils.py
 else
     sed -i '' "s/https:\/\/ghproxy.com\/github.com/https:\/\/github.com/g" modules/launch_utils.py
@@ -263,7 +284,7 @@ source venv/bin/activate
 # Delete pip cache to avoid some errors
 pip cache purge
 
-if ! $net_connected; then
+if ! $network_connected; then
     # Tsinghua mirror has no tb-nightly package, which is needed by basicsr
     # TODO: reset in clean_up
     pip config set global.index-url https://mirrors.aliyun.com/pypi/simple
@@ -283,7 +304,7 @@ fi
 # micromamba install --yes --file requirements_versions.txt
 
 # Fix issue https://github.com/AUTOMATIC1111/stable-diffusion-webui/issues/12210
-if ! $net_connected; then
+if ! $network_connected; then
     echo >>webui-macos-env.sh
     echo "export TORCH_COMMAND=\"pip install wheel==0.41.1 torch==2.0.1 torchvision==0.15.2 cython && pip install git+https://ghproxy.com/https://github.com/TencentARC/GFPGAN.git@8d2447a2d918f8eba5a4a01463fd48e45126a379 --prefer-binary\"" >>webui-macos-env.sh
 fi
